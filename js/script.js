8704d50f1aac4ad053d0af0f5edc81d6defe727e
Remlix <jachan12@s.sfusd.edu>
2019-10-29 01:13:24 +0000

    Worked on inputs

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-$("button").click(function() {
-
-
-
-});
 //GLOBAL VARIABLES
 /* global $ */
-
-
-
+let userInput = "nothing";
+let computerChoice = "nothing";
+let randomNumber = 0;
 // DOCUMENT READY FUNCTION BELOW
+$("button").click(function() {
+    buttoks = $("#input").val()
+    $(" #userChoice").text(buttoks)
+    randomNumber = Math.random()
+    if (randomNumber < .33){
+        computerChoice = "Paper";
+    }else if (randomNumber <.66){}
+        computerChoice = "Rock";
+    }
+    else (randomNumber <.99){
+        computerChoice = "Scissors";
+    }
+
+});
+
 
