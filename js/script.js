a4962376037d3fd961bda26fe22ae94957dcf3ce
Remlix <jachan12@s.sfusd.edu>
2019-11-05 02:12:07 +0000

    Worked on tryin to display inputs glitch

--- js/script.js
+++ js/script.js
@@ -6,17 +6,28 @@ let randomNumber = 0;
 // DOCUMENT READY FUNCTION BELOW
 $("button").click(function() {
     buttoks = $("#input").val()
-    $(" #userChoice").text(buttoks)
+    $("#userChoice").text(buttoks);
     randomNumber = Math.random()
     if (randomNumber < .33){
         computerChoice = "Paper";
-    }else if (randomNumber <.66){}
+    }
+    else if (randomNumber <.66){
         computerChoice = "Rock";
     }
-    else (randomNumber <.99){
+    else if (randomNumber <.99){
         computerChoice = "Scissors";
     }
+    /**
+    display computer choice to screen
+    Display user choice to screen
+     */
 
+//     console.log("success")
+// if (computerChoice === userInput){
+//     console.log("tie!");
+// } else if(computerChoice === paper && userInput === rock){
+//     console.log("computerChoice wins");
+// }
 });
 
 
